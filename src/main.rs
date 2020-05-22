18839bf6a1c2bab95dfe603167b23107be6e0905
Rob Story <wrobstory@gmail.com>
2020-05-22 14:01:30 -0700

    futz around with building a tree from the doc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use hyper::Client;
 use hyper_tls::HttpsConnector;
 
+
+use std::default::Default;
+use std::io::{self, Write};
+
+use scraper::html::Html;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
@@ -9,7 +15,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let part_uri = "https://www.bricklink.com/v2/catalog/catalogitem.page?P=92593".parse()?;
     let resp = client.get(part_uri).await?;
-    println!("{:?}", resp);
+    let doc = hyper::body::to_bytes(resp.into_body()).await?;
+    println!("{:?}", Html::parse_document(&String::from_utf8(doc.to_vec()).unwrap()));
 
     Ok(())
 }
