bea1384cd7f842746664f339e47d503fcdfea3ec
Rob Story <wrobstory@gmail.com>
2020-05-25 09:05:03 -0700

    Vector for collecting known colors works

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,26 @@ use std::time::Instant;
 use hyper::Client;
 use hyper_tls::HttpsConnector;
 
-
 use std::default::Default;
 use std::io::{self, Write};
 use std::str::from_utf8;
 
-use scraper::{Html, Selector, ElementRef, Node};
+use scraper::{ElementRef, Html, Node, Selector};
 
-fn parse_known_colors(part_color_page: &Html) -> () {
+fn parse_known_colors(part_color_page: &Html) -> Vec<&str> {
     let td_selector = Selector::parse(r#"div[class="pciColorTitle"]"#).unwrap();
     let tds = part_color_page.select(&td_selector);
     let known_colors = tds.last().unwrap().next_siblings();
-    for color in known_colors {
-        match color.value() {
-            Node::Element(elem) => println!("{:?}", elem.classes),
-            _ => ()
-        }
-    }
+    known_colors
+        .filter_map(|color_node| match ElementRef::wrap(color_node) {
+            Some(element) => element.text().next(),
+            None => None,
+        })
+        .collect::<Vec<&str>>()
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
     let now = Instant::now();
@@ -37,9 +35,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Byte conversion {}", now.elapsed().as_millis());
     let parsed = Html::parse_document(&String::from_utf8(doc.to_vec()).unwrap());
     println!("doc parsing {}", now.elapsed().as_millis());
-    parse_known_colors(&parsed);
-
-    
+    let colors = parse_known_colors(&parsed);
+    println!("{:?}", colors);
 
     Ok(())
 }
